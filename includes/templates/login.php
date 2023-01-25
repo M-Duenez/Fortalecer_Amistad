a8defbd4383de6c9c0babb6094b5af6625e90be3
Luis-Montes <mecsi_montes@hotmail.com>
2023-01-25 14:01:47 -0600

    oooookey lesgo

--- includes/templates/login.php
+++ includes/templates/login.php
@@ -7,10 +7,7 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../bootstrap/styles.css" rel="stylesheet" type="text/css">
-    <style>
-      body {background-color:#133e35; }
-      #card{--bs-bg-opacity: .5; }
-    </style>
+    <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
     <div class="container-fluid ">
