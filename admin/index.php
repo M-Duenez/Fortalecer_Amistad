55e5901191e73df8abbaea1e34d3f2f784395aa5
M-Duenez <miguel-duenez-1996@outlook.com>
2023-02-01 10:10:46 -0600

    cambios en navbar

--- admin/index.php
+++ admin/index.php
@@ -26,25 +26,47 @@
     <main>
         <div class="container">
             <h1>Lista de Usuarios</h1>
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Nombre Completo</th>
-                    <th scope="col">Clave de Usuario</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php while( $usuarios = mysqli_fetch_assoc($resultado)): ?>
-                    <tr>
-                        <td><?php echo $usuarios['fullname']; ?></td>
-                        <td><?php echo $usuarios['username']; ?></td>
-                        <td><a href="/admin/user_info.php?id=<?php echo $usuarios['id'] ?>" class="boton-amarillo-block">Ver Información</a></td>
-                    </tr>
-                <?php endwhile; ?>
-            </tbody>
-        </table>
+            
+            <!-- DataTales Example -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre Completo</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th scope="col">Nombre Completo</th>
+                                    
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                            <?php while( $usuarios = mysqli_fetch_assoc($resultado)): ?>
+                                <tr>
+                                    <td><?php echo $usuarios['fullname']; ?></td>
+                                    <td>
+                                        <a  class="btn btn-outline-primary">EDITAR</a>
+                                    </td>
+                                    <td>
+                                        <a  class="btn btn-outline-danger">ELIMINAR</a>
+                                    </td>
+                                    <td>
+                                        <a href="/admin/user_info.php?id=<?php echo $usuarios['id'] ?>" class="btn btn-outline-success">Ver Información</a>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>                                         
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </main>    
 </body>
